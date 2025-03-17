d232773085277fdb3fb9651b888dcc8085cca250
MLASS-Dilshan <sachira.sandeepa14@gmail.com>
2025-03-17 14:41:31 +0530

    added smooth animation

--- navbar/src/components/Header.jsx
+++ navbar/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 
   return (
     <header>
-      <nav className="flex justify-between items-center max-w-[1920px] mx-auto px-[10%] h-[80px] bg-blue-500 text-[#fff]">
+      <nav className="flex justify-between items-center max-w-[1920px] mx-auto px-[10%] h-[80px] bg-blue-500 text-[#fff] relative">
         <p className="text-[30px] font-bold">Sachira</p>
 
         <div className="md:hidden">
@@ -30,11 +30,20 @@ const Header = () => {
           <li>About</li>
           <li>Contact</li>
         </ul>
+
+        <div className={`md:hidden bg-blue-400 rounded-b-[20px] absolute top-20  left-0 w-full duration-400 ease-in-out z-[-10] ${isOpen ? 'top-20' : 'top-[-450px]'}`}>
+          <ul className="flex flex-col items-center gap-[20px] py-[20px] pb-[20px] text-[20px] text-[#fff] ">
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">About</li>
+            <li className="cursor-pointer">Contact</li>
+          </ul>
+        </div>
+     
       </nav>
 
       {/* Mobile Menu */}
 
-      {isOpen ? (
+      {/* {isOpen ? (
         <div className="md:hidden bg-blue-500 rounded-b-[20px]">
           <ul className="flex flex-col items-center gap-[20px] pb-[20px] text-[20px] text-[#fff] ">
             <li className="cursor-pointer">Home</li>
@@ -42,7 +51,7 @@ const Header = () => {
             <li className="cursor-pointer">Contact</li>
           </ul>
         </div>
-      ) : null}
+      ) : null} */}
     </header>
   );
 };
